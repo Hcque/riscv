84174b483b69b5545bcbbd05c65c987d6e682aea
Hcque <hc995639@gmail.com>
2022-01-02 20:58:28 +0800

    pred

--- src/forward.hpp
+++ src/forward.hpp
@@ -1,7 +1,13 @@
 
 
+#pragma once
+
+#include "Common.hpp"
+
 void forward(MemoryAccess& ma, Execution& ex)
 {
+
+    // if (ex.inst.rd !=0 && ex.inst.rs1 == id.inst.rs1)
     // ma.inst.src1 = ...;
     // ma.inst.src2 = ...;
 
@@ -15,6 +21,7 @@ void forward(MemoryAccess& ma, InstructionDecode& id)
 
 void forward(Execution& ex, InstructionDecode& id)
 {
-
+    if (ex.inst.rd !=0 && ex.inst.rd == id.inst.rs1) id.regs->set( ex.regs->get( ex.inst.rd ) );
+    if (ex.inst.rd !=0 && ex.inst.rd == id.inst.rs2) id.regs->set( ex.regs->get( ex.inst.rd ) );
 }
 
--- src/prediction.hpp
+++ src/prediction.hpp
@@ -4,23 +4,86 @@
 #include <unordered_map>
 
 
-class Pattern{
+class StaticPred{
+public:
+    int tot, hit;
+    Static(): tot(0), hit(0){}
+
+    bool take()   
+    {
+        return 0;
+    }
+
+    vold validate(bool jump)
+    {
+        tot++;
+        if (!jump) hit ++;
+    }
 
 };
 
-class AdptiveTrain
+class TwobitPred
+{
+public:
+    bool b0, b1;
+    int tot, hit;
+    bool pred;
+    Static(): tot(0), hit(0), b0(1), b1(0){}
+
+    void increase()
+    {
+        if (b1 && b0) return;
+        else if ( !b1 && b0) b1 = 1;
+        else if (b1 && !b0) b1 = 0, b0 = 1;
+        else if (!b1 && !b0) b1 = 1, b0 = 0;
+        else assert(0);
+    }
+    void decrease()
+    {
+        if (b1 && b0) b1 = 0;
+        else if ( !b1 && b0) b1 = 1, b0 = 0;
+        else if (b1 && !b0) b1 = 0;
+        else if (!b1 && !b0) return;
+        else assert(0);
+    }
+
+    bool take()   
+    {
+        return b0;
+    }
+
+    vold validate(bool jump)
+    {
+        tot++;
+        if (jump == b0)  // pred correct
+        {
+            hit ++;
+        }
+        if (jump) decrease();
+        else increase();
+    }
+
+};
+
+
+class AdptiveTrain // 
 {
     bool b0, b1;
-    bool query()   
+    bool take()   
     {
         return 0;
+    }
+
+    vold validate(bool jump)
+    {
+
     }
 
 };
 
 class Prediction
 {
-    std::unordered_map<uint32_t, Pattern> strmap;
+    std::unordered_map<uint32_t, TwobitPred> strmap;
     std::queue<bool> predictPool;
 
     bool take()
